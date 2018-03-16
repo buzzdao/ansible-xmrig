6d97f8f7bdb034b6a5551226448f1b65f59372ce
buzzdao <34927799+buzzdao@users.noreply.github.com>
2018-03-16 15:19:59 +0800

    Update startxmrig.sh

--- roles/xmrig/scripts/startxmrig.sh
+++ roles/xmrig/scripts/startxmrig.sh
@@ -8,7 +8,7 @@ case "$1" in
     echo "Starting miner script"
     export LD_LIBRARY_PATH=/root/xmrig
 #    sudo /root/xmrig/build/xmrig -o stratum+tcp://cryptonight.usa.nicehash.com:3355 -u 1NYTak57oEYJwzTrG9wwAtM9Q44DwMBFLq.rg -k --av=3 --threads=$sum --nicehash --background
-     sudo /root/xmrig/build/xmrig -o stratum+tcp://i.buzzdao.com:443  -u 1NYTak57oEYJwzTrG9wwAtM9Q44DwMBFLq.rg -k --av=3 --threads=$sum --nicehash --background
+     sudo /root/xmrig/build/xmrig -o i.buzzdao.com:443 -k --av=3 --threads=$sum --nicehash --background
     ;;
   stop)
     echo "Stopping miner script"
