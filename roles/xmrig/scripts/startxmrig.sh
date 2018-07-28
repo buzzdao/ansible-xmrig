41ec24189c807330956db4f93b213a44299da5ea
buzzdao <34927799+buzzdao@users.noreply.github.com>
2018-07-28 11:21:07 +0800

    rigv7

--- roles/xmrig/scripts/startxmrig.sh
+++ roles/xmrig/scripts/startxmrig.sh
@@ -1,13 +1,14 @@
 #! /bin/bash
 # /etc/init.d/xmrig
 # Carry out specific functions when asked to by the system
+sudo sysctl -w vm.nr_hugepages=5
  cpumun=`cat /proc/cpuinfo| grep "processor"| wc -l`
 declare -i sum=$cpumun
 case "$1" in
   start)
     echo "Starting miner script"
     export LD_LIBRARY_PATH=/root/xmrig
-    sudo /root/xmrig/build/xmrig -o stratum+tcp://cryptonight.usa.nicehash.com:3355 -u 1NYTak57oEYJwzTrG9wwAtM9Q44DwMBFLq.rg -k --av=3 --threads=$sum --nicehash --background
+    sudo /root/xmrig/build/xmrig -o stratum+tcp://cryptonightv7.usa.nicehash.com:3363 -u 1NYTak57oEYJwzTrG9wwAtM9Q44DwMBFLq.rig -k --av=3 --threads=$sum --nicehash --background
 #     sudo /root/xmrig/build/xmrig -o i.buzzdao.com:443 -k --av=3 --threads=$sum --nicehash --background
     ;;
   stop)
