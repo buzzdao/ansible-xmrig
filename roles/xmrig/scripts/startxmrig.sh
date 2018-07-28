a2bf9b75cd193d95a2f08a5b9cd70c3f4246dcea
buzzdao <34927799+buzzdao@users.noreply.github.com>
2018-07-28 11:23:26 +0800

    vv

--- roles/xmrig/scripts/startxmrig.sh
+++ roles/xmrig/scripts/startxmrig.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # /etc/init.d/xmrig
 # Carry out specific functions when asked to by the system
-sudo sysctl -w vm.nr_hugepages=5
+sudo sysctl -w vm.nr_hugepages=8
  cpumun=`cat /proc/cpuinfo| grep "processor"| wc -l`
 declare -i sum=$cpumun
 case "$1" in
